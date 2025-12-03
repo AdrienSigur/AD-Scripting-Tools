dc64a52b9b430fb2c90e4ebaeb4aa83360cede48
unknown <sigur.adrien@gmail.com>
2025-12-03 14:56:52 +0100

    ram used

--- Process.ps1
+++ Process.ps1
@@ -1,3 +1,26 @@
 Get-Process | Sort-Object -Property CPU -Descending  | Select-Object -First 5 
 
 ## Cpu process 
+
+
+$totalCpu = 0 
+
+Get-Process | ForEach-Object {
+    $totalCpu += $_.CPU
+}
+
+
+$os = Get-CimInstance Win32_ComputerSystem
+$ram = Get-CimInstance -ClassName CIM_OperatingSystem
+
+
+
+$ramTotal = $os.TotalPhysicalMemory
+$ramfree = $ram.FreePhysicalMemory
+$UsedRam = [math]::Round((($ramTotal - $ramFree) / $ramTotal) * 100, 2)
+
+
+
+Write-Host
+
+Write-Host "Ram total used = $UsedRam"

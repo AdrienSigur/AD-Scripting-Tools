1709c216254d1a249738fdb54573d66cb893cf6c
unknown <sigur.adrien@gmail.com>
2025-12-16 13:05:20 +0100

    Add User Automaticly in active directory

--- AddUser.ps1
+++ AddUser.ps1
@@ -5,32 +5,39 @@ $CsvPath="User.csv"
 
 $domainRoot = (Get-AdDomain).DistinguishedName
 
+$dnsRoot = (Get-AdDomain).dnsRoot
+
 $defaultPassword = "Root1234@"
 
 $securePassword = ConvertTo-SecureString $defaultPassword -AsPlainText -Force 
 
-$users = Import-Csv $CsvPath -Delimiter "," | Select-Object -First 1
+$users = Import-Csv $CsvPath -Delimiter "," 
 
 
- foreach($user in $users){
-
-
-
-    $SamName = "$($user.Prenom)"
-e
-    $SamLastName = "$($user.Nom)"
-
-    $SamAccountName = $a.tolower().Substring(0,1)+"."+$last.tolower()
+foreach($user in $users){
 
     
-      $SamAccountName = 
+  $SamName = "$($user.Prenom)"
 
-      $UserCreation = @{
-        DistinguishedName = "OU=$($user.SousOu), $OUPrincipal , $domainRoot"
-        Name = "$user.Prenom"
+    $SamLastName = "$($user.Nom)"
+
+    $SamAccountName = $SamName.tolower().Substring(0,1)+"."+$SamLastName.tolower()
+
+    
+
+    $UserCreation = @{
+        Path = "OU=$($user.SousOu),$OUPrincipal,$domainRoot"
+        Name = "$($user.Prenom)"
         SamAccountName = $SamAccountName
+        UserPrincipalName = "${SamAccountName}@$dnsRoot"
+        ChangePasswordAtLogon = $true
+        Department =  "$($user.SousOU)"
+        EmailAddress  = "${SamAccountName}@$dnsRoot"
         AccountPassword = $securePassword
-
+        Enabled = $true
      }
-     New-AdUser @UserCreation
+     
+    New-AdUser @UserCreation
+
+     Write-host "$($User.Prenom) is success and created" -ForegroundColor Green 
  }
